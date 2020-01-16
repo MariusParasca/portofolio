ed9f8dcaa76d74d6f5f18c2935efcca0e5c987a6
mariusparasca <mrs.parasca@gmail.com>
2020-01-16 20:27:56 +0200

    update title

--- src/components/ApplicationBar/ToolBarContent/ToolBarContent.js
+++ src/components/ApplicationBar/ToolBarContent/ToolBarContent.js
@@ -47,7 +47,7 @@ const ToolBarContent = props => {
           root: styles.typographyRoot,
         }}
       >
-        Marius Parasca Portofolio
+        Marius Parasca | Portofolio
       </Typography>
       <div className={classes.gitHubIconContainer}>
         <GitHubIcon link="https://github.com/MariusParasca" text="GitHub" />
