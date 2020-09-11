1ed852e2166731ab1f5062e5f1e3fda775af3e70
marius <mrs.parasca@gmail.com>
2020-09-11 09:58:39 +0300

    added google analitics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
+import ReactGA from 'react-ga';
 
 import ApplicationBar from 'components/ApplicationBar/ApplicationBar';
 import Main from 'components/Main/Main';
@@ -7,6 +8,13 @@ import Projects from 'components/Projects/Projects';
 import Footer from 'components/Footer/Footer';
 
 import theme from './theme';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-177805206--1');
+  ReactGA.pageview('/');
+}
+
+initializeReactGA();
 
 function App() {
   return (
